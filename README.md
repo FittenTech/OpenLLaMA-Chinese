9b49f6dd0d3b92621dd83084b1e9d8c7b82f9b15
li-xl <1905692338@qq.com>
2023-06-12 12:50:58 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# OpenAlpaca-Chinese
+# OpenLLaMA-Chinese
+
+![](media/logo.webp)
+
+[OpenLLaMA](https://github.com/openlm-research/open_llama) released a permissively licensed open source reproduction of Meta AI’s LLaMA 7B and 13B trained on the RedPajama dataset and a smaller 3B variant of LLaMA model.
+We have conducted fine-tuning on Chinese and English instructions based on it and have publicly released our weights.
+
+Chinese instructions finetuning weights:
+
+- [OpenLLaMA 3B](https://huggingface.co/FittenTech/openllama-chinese-3b)
+- [OpenLLaMA 7B](https://huggingface.co/FittenTech/openllama-chinese-7b)
+- [OpenLLaMA 13B](coming soon!)
+
+English instructions finetuning weights:
+- [OpenLLaMA 3B](coming soon!)
+- [OpenLLaMA 7B](coming soon!)
+- [OpenLLaMA 13B](coming soon!)
+
+Chinese+English instructions finetuning weights:
+- [OpenLLaMA 3B](coming soon!)
+- [OpenLLaMA 7B](https://huggingface.co/FittenTech/openllama-chinese-english-7b)
+- [OpenLLaMA 13B](coming soon!)
+
+## Data
+
+For Chinese finetuning, we used the [alpaca_data_zh_51k.json](data/alpaca_data_zh_51k.json) from [Chinese-LLaMA-Alpaca](https://github.com/ymcui/Chinese-LLaMA-Alpaca).
+
+For English finetuning, we used the [alpaca_data.json](data/alpaca_data.json) from [StanfordAlpaca](https://github.com/tatsu-lab/stanford_alpaca).
+
+For English+Chinese finetuning, we used both of them.
+
+## Usage
+We provide inference code based on [Jittor](https://github.com/Jittor/jittor) and [torch](https://github.com/pytorch/pytorch)
+
+### Jittor Inference
+coming soon.
+
+### Torch Inference
+We modified the generate code from [LLaMA-X](https://github.com/AetherCortex/Llama-X).
+
+Download the weights and change the `base_model` path in `inference/gen_torch.py`
+```shell
+python inference/gen_torch.py
+```
+
+## Pretraining and Finetuning
+FittenTech offers LLMs pretraining and fine-tuning services. Further details can be found [here](https://llm.fittentech.com/).
+
+## Thanks
+We would like to express our gratitude to the developers of the following open-source projects, as our project is based on their work.
+
+- [LLaMA](https://github.com/facebookresearch/llama)
+- [OpenLLaMA](https://github.com/openlm-research/open_llama)
+- [Jittor](https://github.com/Jittor/jittor)
+- [JittorLLMs](https://github.com/Jittor/JittorLLMs)
+- [LLaMA-X](https://github.com/AetherCortex/Llama-X)
+- [Chinese-LLaMA-Alpaca](https://github.com/ymcui/Chinese-LLaMA-Alpaca)
+- [StanfordAlpaca](https://github.com/tatsu-lab/stanford_alpaca)
+- [transformers](https://github.com/huggingface/transformers)
+
+## License
+We adopt the Apache License, following OpenLLaMA's license.
---
+++ inference/gen_torch.py
@@ -0,0 +1,117 @@
+import sys
+import fire
+import torch
+import transformers
+import gradio as gr
+from transformers import LlamaTokenizer, LlamaForCausalLM, GenerationConfig
+
+
+
+def main(
+    load_8bit: bool = False,
+    base_model: str = "/mnt/disk1/lxl/release_models/openllama-chinese-7b"
+):
+    tokenizer = LlamaTokenizer.from_pretrained(base_model)
+    model = LlamaForCausalLM.from_pretrained(
+        base_model,
+        load_in_8bit=load_8bit,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+
+    # unwind broken decapoda-research config
+    model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
+    model.config.bos_token_id = 1
+    model.config.eos_token_id = 2
+
+    if not load_8bit:
+        model.half() 
+
+    model.eval()
+    if torch.__version__ >= "2" and sys.platform != "win32":
+        model = torch.compile(model)
+
+    def evaluate(
+        instruction,
+        input=None,
+        temperature=0.6,
+        top_p=0.9,
+        top_k=40,
+        num_beams=4,
+        max_new_tokens=512,
+        **kwargs,
+    ):
+        prompt = generate_prompt(instruction, input)
+        inputs = tokenizer(prompt, return_tensors="pt")
+        input_ids = inputs["input_ids"].to(model.device)
+        generation_config = GenerationConfig(
+            temperature=temperature,
+            top_p=top_p,
+            top_k=top_k,
+            num_beams=num_beams,
+            **kwargs,
+        )
+        with torch.no_grad():
+            generation_output = model.generate(
+                input_ids=input_ids,
+                generation_config=generation_config,
+                return_dict_in_generate=True,
+                output_scores=True,
+                max_new_tokens=max_new_tokens,
+            )
+        s = generation_output.sequences[0]
+        output = tokenizer.decode(s)
+        return output.split("### Response:")[1].strip()
+
+    gr.Interface(
+        fn=evaluate,
+        inputs=[
+            gr.components.Textbox(
+                lines=2, label="Instruction", placeholder="Tell me about alpacas."
+            ),
+            gr.components.Textbox(lines=2, label="Input", placeholder="none"),
+            gr.components.Slider(minimum=0, maximum=1, value=0.1, label="Temperature"),
+            gr.components.Slider(minimum=0, maximum=1, value=0.75, label="Top p"),
+            gr.components.Slider(
+                minimum=0, maximum=100, step=1, value=40, label="Top k"
+            ),
+            gr.components.Slider(minimum=1, maximum=4, step=1, value=4, label="Beams"),
+            gr.components.Slider(
+                minimum=1, maximum=2000, step=1, value=128, label="Max tokens"
+            ),
+        ],
+        outputs=[
+            gr.inputs.Textbox(
+                lines=5,
+                label="Output",
+            )
+        ],
+        title="OpenLLaMA-Chinese",
+        description="Improve OpenLLaMA model to follow chinese instructions. If your instruction does not require any input, please leave the input field empty.",
+    ).launch(share=True)
+
+
+def generate_prompt(instruction, input=None):
+    if input:
+        return f"""Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{instruction}
+
+### Input:
+{input}
+
+### Response:
+"""
+    else:
+        return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+### Instruction:
+{instruction}
+
+### Response:
+"""
+
+
+if __name__ == "__main__":
+   fire.Fire(main)
